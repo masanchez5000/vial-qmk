a7d36a1b3f25a44c41493f98a9caae8a7be8f4ed
masanchez5000 <masanchez5000@gmail.com>
2022-04-24 23:31:34 +0200

    vial dz60

---
+++ keyboards/dz60/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+#define VIAL_KEYBOARD_UID {0x26, 0xFB, 0xE4, 0x4A, 0x5F, 0x78, 0x51, 0x21}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
